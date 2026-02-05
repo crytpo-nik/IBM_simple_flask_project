a53385b8c523cad83e05976d75ce258db8845d4d
crytpo-nik <crypto.niiik@gmail.com>
2026-02-05 11:01:37 +0100

    3rd commit

--- server.py
+++ server.py
@@ -1,13 +1,24 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from EmotionDetection.emotion_detection import emotion_detector
 
 app = Flask(__name__)
 
-@app.route("/emotionDetector")
-def rend_indec_page():
+@app.route("/")
+def rend_index_page():
     return render_template("index.html")
 
+@app.route("/emotionDetector", methods=["GET"])
 
+def detect_emotion():
+
+    text_to_analyze = request.args.get("textToAnalyze")
+
+    if not text_to_analyze:
+        return "No text provided", 400
+
+    result = emotion_detector(text_to_analyze)
+
+    return jsonify(result)
 
 if __name__ == "__main__":
     app.run(debug=True)
